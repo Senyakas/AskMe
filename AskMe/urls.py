c3892fea0e8f3044246c967915c8900eceff0e6b
Арсений Чистяков <senyaaa@MacBook-Air-Arsenij.local>
2023-11-14 21:30:22 +0300

    improved tag search

--- AskMe/urls.py
+++ AskMe/urls.py
@@ -21,10 +21,11 @@ from app import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('question/<int:question_id>', views.question, name = 'question'),
-    path('tag', views.tag_listing, name = 'tag_listing'),
+    path('tag/<str:tag>', views.tag_listing, name = 'tag_listing'),
     path('ask', views.ask, name = 'ask'),
     path('settings', views.settings, name = 'settings'),
     path('login', views.login, name = 'login'),
     path('signup', views.signup, name = 'signup'),
+    path('hot', views.hot_questions, name = 'hot_questions'),
     path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -2,20 +2,34 @@ from django.http import request
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
-QUESTIONS = [
-    {
-        'id': i,
-        'title': 'Question ' + str(i),
-        'content': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et'
-    } for i in range(100)
-]
-
 ANSWERS = [
     {
         'id': i,
         'content': 'Lorem ipsum dolor graecis sollicitudin agam scripserit saperet partiendo adipisci reque pharetra noluisse maluisset eius neglegentur suas causae sem convallis commune constituto comprehensam contentiones ludus assueverit porro tortor pellentesque elaboraret wisi semper causae postulant diam cubilia dicant idque viverra vim conclusion emque suas quidamsit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren'
     } for i in range(100)
 ]
+
+QUESTIONS = [
+    {
+        'id': i,
+        'title': 'Question ' + str(i),
+        'content': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+        'tags' : [],
+    } for i in range(100)
+]
+
+for i in range(100):
+    if i % 2 == 0:
+        QUESTIONS[i]['tags'] = ['C++', 'Databases']
+    else:
+        QUESTIONS[i]['tags'] = ['CSS', 'HTML']
+    if i % 3 == 0:
+        QUESTIONS[i]['tags'].append('Python')
+    if i % 5 == 0:
+        QUESTIONS[i]['tags'].append('Django')
+    if i % 7 == 0:
+        QUESTIONS[i]['tags'].append('Bootstrap')
+
 def paginate(objects, request, per_page = 15):
     paginator = Paginator(objects, per_page)
     page = request.GET.get('page', 1)
@@ -27,8 +41,13 @@ def index(request):
 def question(request, question_id):
     item = QUESTIONS[question_id]
     return render(request, 'question.html', {'question': item, 'answers': paginate(ANSWERS, request, 5)})
-def tag_listing(request):
-    return render(request, 'tag_listing.html', {'questions' : paginate(QUESTIONS, request)})
+
+def tag_listing(request, tag):
+    requested_questions = []
+    for item in QUESTIONS:
+        if tag in item['tags']:
+            requested_questions.append(item)
+    return render(request, 'tag_listing.html', {'questions' : paginate(requested_questions, request), 'tag' : tag})
 
 def ask(request):
     return render(request, 'ask.html')
@@ -41,3 +60,6 @@ def login(request):
 
 def signup(request):
     return render(request, 'signup.html')
+
+def hot_questions(request):
+    return render(request, 'hot_questions.html', {'questions' : paginate(QUESTIONS, request)})
