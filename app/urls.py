2191842fad6d1205e5cde39f7911dbece7856d0d
Арсений Чистяков <senyaaa@Air-Arsenij.Dlink>
2023-11-15 21:28:03 +0300

    added urls.py in app directory, fixed some files

--- app/urls.py
+++ app/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', include('app.urls')),
+    path('', views.index, name = 'index'),
+    path('question/<int:question_id>', views.question, name = 'question'),
+    path('tag/<str:tag>', views.tag_listing, name = 'tag_listing'),
+    path('ask', views.ask, name = 'ask'),
+    path('settings', views.settings, name = 'settings'),
+    path('login', views.login, name = 'login'),
+    path('signup', views.signup, name = 'signup'),
+    path('hot', views.hot_questions, name = 'hot_questions'),
     path('admin/', admin.site.urls),
 ]
